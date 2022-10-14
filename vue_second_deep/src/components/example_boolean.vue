8a27e7fbe4542be072c8cb4daa186375cc85493f
Alice Medvedeva <alisa.medvedeva@optimax.dev>
2022-10-14 22:46:18 +0500

    second example

---
+++ vue_second_deep/src/components/example_boolean.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- В этом случае canShow будет равен true внутри TestComponent -->
+    <TestComponent canShow="" />
+
+    <!-- Этот пример аналогичен предыдущему, vue-template-compiler выставит пустую строку для нашего prop'а -->
+    <TestComponent canShow />
+
+    <!-- Тут canShow тоже равен true -->
+    <TestComponent canShow="can-show" />
+    <TestComponent canShow="canShow" />
+    <TestComponent canShow="dftgyhu" />
+    <TestComponent :canShow="null" />
+  </div>
+</template>
+
+<script>
+import TestComponent from './test_component';
+
+export default {
+  components: {
+    TestComponent,
+  },
+};
+</script>
