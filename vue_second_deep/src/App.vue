503022497eb11e92ff70c59a4ad99ffde9fcd401
Alice Medvedeva <alisa.medvedeva@optimax.dev>
2022-10-14 22:04:58 +0500

    first example

---
+++ vue_second_deep/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ExampleFirst />
+  </div>
+</template>
+
+<script>
+import ExampleFirst from './components/example_first';
+
+export default {
+  name: 'App',
+  components: {
+    ExampleFirst
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
